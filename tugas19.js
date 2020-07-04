8c8214bdc92b74c7d0bcbb9d14e78d1bce101e35
hugoiman <hugogiman17@gmail.com>
2020-07-04 14:01:22 +0700

    Tugas 19 Javascript

---
+++ tugas19.js
@@ -0,0 +1,13 @@
+var word = "Semangat...";
+console.log(word.repeat(5));
+
+var stars = "*****";
+console.log(stars.repeat(1));
+console.log(stars.repeat(3));
+console.log(stars.repeat(5));
+console.log(stars.repeat(7));
+console.log(stars.repeat(9));
+console.log(stars.repeat(7));
+console.log(stars.repeat(5));
+console.log(stars.repeat(3));
+console.log(stars.repeat(1));
